3991e10e767f83b48bb178c89289c29291d81890
Shashwatshah02 <shashwatshah02@gmail.com>
2024-02-11 11:56:39 +0530

    FINAL

--- src/components/Upcoming_Events.jsx
+++ src/components/Upcoming_Events.jsx
@@ -74,8 +74,9 @@ const Upcoming_Events = () => {
           <Heading size="lg">Welcome {response.username}</Heading>
           {/* Add your dashboard content here */}
           <Box bg="gray.100" w="100%" p="2%" height={"80vh"}>
-            <Calendar apiKey={API_KEY} calendars={calendars} />
-            <a target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=N3FqcjQ1OWpsNWl1dWlzM2ZxOGNiOGY4NGwgZ2F1cmliNDlAbQ&amp;tmsrc=gaurib49%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en-GB.gif"/></a>
+            {/* <Calendar apiKey={API_KEY} calendars={calendars} />
+            <a target="_blank" href="https://calendar.google.com/calendar/event?action=TEMPLATE&amp;tmeid=N3FqcjQ1OWpsNWl1dWlzM2ZxOGNiOGY4NGwgZ2F1cmliNDlAbQ&amp;tmsrc=gaurib49%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en-GB.gif"/></a> */}
+            <iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Asia%2FKolkata&bgcolor=%23ffffff&src=c2hhc2h3YXRzaGFoMDJAZ21haWwuY29t&src=MDlmYWVlMzk1MzEwZTJmZjczNTlhODM2YTEwZTYwNGM1NjRjNmJhMTcyNzRkMjA3YWIzYmVjN2JjMzNiOTQzM0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=Y2xhc3Nyb29tMTAyODQ2ODM2OTE5NzcxODM5OTQyQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2xhc3Nyb29tMTE4NDIwODEwOTYzNTQxMTQ5MjE5QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2xhc3Nyb29tMTE3NjAzNjA3MjE3OTk1MjcxODI3QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=Y2xhc3Nyb29tMTE1Mzc4NDgyMDM3NzA5Mzc4MTMzQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=ZW4tZ2IuaW5kaWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=Y2xhc3Nyb29tMTE1NzUzOTUyNDc2NTk0NjA2MDAyQGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&src=Z2F1cmliNDlAZ21haWwuY29t&src=Y2xhc3Nyb29tMTAzMjM1Mjk1OTg0Nzk0OTk0NzA3QGdyb3VwLmNhbGVuZGFyLmdvb2dsZS5jb20&color=%23039BE5&color=%23D81B60&color=%230047a8&color=%230047a8&color=%23c26401&color=%23202124&color=%230B8043&color=%23c26401&color=%23E4C441&color=%23137333" style={{border:"solid 1px #777", width:"100%", height:"70vh", frameborder:"0", scrolling:"no"}}></iframe>
           </Box>
         </VStack>
       </Box>
